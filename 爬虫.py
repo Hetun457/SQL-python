08b7e554bc0d488098bb0e7d142c836841bebb98
Hetun457 <biyth10@nottingham.edu.cn>
2026-06-29 17:25:37 +0800

    Update 爬虫.py

--- 爬虫.py
+++ 爬虫.py
@@ -3,67 +3,69 @@ import requests
 import json
 import re
 
-db = MySqlHelper(
-    host="localhost",
-    user="root",
-    password="",
-    database="studenttable" 
-)
+class BaiduSpider:
 
-def get_baidu_hot():
-
-    url = "https://top.baidu.com/board?tab=realtime"
-
-    headers = {
-        "User-Agent":"Mozilla/5.0"
-    }
-
-    resp = requests.get(url, headers=headers)
-    resp.encoding = "utf-8"
-    html = resp.text
-
-    result = re.search(r'<!--s-data:(.*?)-->', html, re.S)
-
-    if result is None:
+    def __init__(self):
+        self.db =  MySqlHelper(
+            host="localhost",
+            user="root",
+            password="",
+            database="studenttable" 
+        )
+        
+        self.url = "https://top.baidu.com/board?tab=realtime"
+        self.headers = {"User-Agent":"Mozilla/5.0"}
+        
+    def get_baidu_hot(self):
+        resp = requests.get(url, headers=headers)
+        resp.encoding = "utf-8"
+        html = resp.text
+        
+        result = re.search(r'<!--s-data:(.*?)-->', html, re.S)
+        
+        if result is None:
         print("没有找到数据")
         return []
 
-    json_text = result.group(1)
+        json_text = result.group(1)
+        data = json.loads(json_text)
+        hot_list = data["data"]["cards"][0]["content"]
+        data_list = []
 
-    data = json.loads(json_text)
+        for item in hot_list[:10]:
+            rank = item["index"]
+            title = item["word"]
+            
+            try:
+                hot = int(item["hotScore"])
+            except:
+                hot = 0
+            
+            data_list.append([rank, title, hot])
+        
+        return data_list
 
-    hot_list = data["data"]["cards"][0]["content"]
+def save_data(self, hot_data):
 
-    data_list = []
+        insert_sql = """
+        INSERT INTO baidu_hot(hot_rank, title, hot_value)
+        VALUES (%s, %s, %s)
+        """
 
-    for item in hot_list[:10]:
+        for item in hot_data:
+            self.db.execute(insert_sql, item)
+            print(f"成功存入：第{item[0]}名 {item[1]}")
+            
+ def run(self):
+        hot_data = self.get_baidu_hot()
 
-        rank = item["index"]
-        title = item["word"]
-
-        try:
-            hot = int(item["hotScore"])
-        except:
-            hot = 0
-
-        data_list.append([rank, title, hot])
-
-    return data_list
-
+        if hot_data:
+            self.save_data(hot_data)
+        self.db.close()
+        print("successful saved")
 
 if __name__ == "__main__":
+    spider = BaiduSpider()
+    spider.run()
 
-    hot_data = get_baidu_hot()
-
-    insert_sql = """
-    INSERT INTO baidu_hot(hot_rank, title, hot_value)
-    VALUES (%s, %s, %s)
-    """
-
-    for item in hot_data:
-        db.execute(insert_sql, item)
-        print(f"成功存入：第{item[0]}名 {item[1]}")
-
-    db.close()
-
-    print("successful saved")
+   
